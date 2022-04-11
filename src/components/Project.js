4eb4cb523b55150648833ab7316acbaade95b09b
C Bares <colinedwinbares@gmail.com>
2022-04-10 20:29:23 -0500

    styling portfolio page

--- src/components/Project.js
+++ src/components/Project.js
@@ -3,19 +3,27 @@ const Project = (props) => {
   const { title, url, description, stack, deployed, github } = props.project
 
   return (
-    <div className='project'>
+    <section className='project'>
       <img src={url} alt="" className='screenshot'/>
-      <h2>{title}</h2>
-      <p>{description}</p>
-      <h3>Tech Stack</h3>
-      <p>{stack}</p>
-      <p>
-        <a href={deployed}>Live</a>
-      </p>
-      <p>
-        <a href={github}>Repo</a>
-      </p>
-    </div>
+      <div className='project-text'>
+        <div className='title-line'>
+          <h2>{title}</h2>
+          <p>{description}</p>
+        </div>
+        <div className='tech-line'>
+          <h3>Tech Stack</h3>
+          <p>{stack}</p>
+        </div>
+        <div className='link-line'>
+          <p>
+            <a href={deployed}>Live</a>
+          </p>
+          <p>
+            <a href={github}>Repo</a>
+          </p>
+        </div>
+      </div>
+    </section>
   );
 }
 
--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,29 +1,26 @@
 const Resume = () => {
   return (
-    <section>
-      <h2>Strengths</h2>
-      <h3>Reliability</h3>
-      <p>
-      In my six years as a Team Lead at <a href="https://www.badgerinventory.com/">Badger Inventory</a> I leanred what it means to be reliable in a constantly changing environment. Odd hours, unpredictable schedules, driving coworkers long distance through every kind of weather, different stores with different expectations every day, extreme attention to detail, and working fully independently through different store departments and returning reliable numbers in often complex situations.
-      </p>
-      <h3>Flexibility</h3>
-      <p>At Badger, I learned to how to apply my basic understanding of the equipment and workflow to a new environment every day. Every manager at every store had unique demands and expectations.</p>
-      <p>You can find my full resume <a href="/">here</a> </p>
-      <h3>Creativity!</h3>
-      <p>
-        I've been a part-time musician and some-times visual artist all my adult life. From{' '}
-        <a href="https://colinedwinbares.bandcamp.com/">home recording</a> to{' '}
-        <a href="https://soundproofboot.github.io/random-color-test">
-          {' '}
-          generative art{' '}
-        </a>{' '}
-        to{''}
-        <a href="https://www.youtube.com/watch?v=2xJsu071O2c">
-          video editing,
-        </a>{' '}
-        I'm passionate about learning new technologies to express new ideas.
-      </p>
-      </section>
+    <section id='resume-section'>
+    
+    <h2>
+      Front End Proficiencies
+      </h2>
+      <ul>
+        <li>HTML/CSS/JS</li>
+        <li>React</li>
+        <li>Responsive Design</li>
+      </ul>
+
+      <h2>Back End Proficiencies</h2>
+      <ul>
+        <li>APIs</li>
+        <li>Node</li>
+        <li>Express.js</li>
+        <li>MySQL/Sequelize</li>
+        <li>MongoDB/Mongoose</li>
+      </ul>
+        <p id='link-line'>You can find my full resume <a href="#">here</a>.</p>      
+    </section>
       );
     };
     
