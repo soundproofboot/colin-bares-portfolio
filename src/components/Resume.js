0c26b75ea219f2071671ed8c139ac312b478d91c
C Bares <colinedwinbares@gmail.com>
2022-05-23 13:58:18 -0500

    linked to resume on google drive

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -1,10 +1,7 @@
 const Resume = () => {
   return (
-    <section id='resume-section'>
-    
-    <h2>
-      Front End Proficiencies
-      </h2>
+    <section id="resume-section">
+      <h2>Front End Proficiencies</h2>
       <ul>
         <li>HTML/CSS/JS</li>
         <li>React</li>
@@ -19,9 +16,15 @@ const Resume = () => {
         <li>MySQL/Sequelize</li>
         <li>MongoDB/Mongoose</li>
       </ul>
-        <p id='link-line'>You can find my full resume <a href="#">here</a>.</p>      
+      <p id="link-line">
+        You can find my full resume{' '}
+        <a target="_blank" href="https://drive.google.com/file/d/1yDTvJC3a55dGo_Im9LBA75LMep06iwuC/view?usp=sharing">
+          here
+        </a>
+        .
+      </p>
     </section>
-      );
+  );
     };
     
     export default Resume;
