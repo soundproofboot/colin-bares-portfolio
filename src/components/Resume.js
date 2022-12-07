9f2f09da855c401851db1a94455aebedd2769e66
C Bares <colinedwinbares@gmail.com>
2022-12-07 13:43:19 -0600

    update resume link and technical skills section of Resume.js

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -4,18 +4,19 @@ const Resume = () => {
       <h2>Technical Skills</h2>
       <ul>
         <li>React</li>
-        <li>MongoDB/MySQL</li>
+        <li>MongoDB/MySQL/Supabase</li>
         <li>GraphQL/RESTful APIs with Express.js</li>
         <li>Git and GitHub</li>
-        <li>HTML5/CSS3/modern JavaScript</li>
+        <li>HTML5/CSS3/JavaScript/TypeScript</li>
+        <li>AWS/Google Cloud Platform</li>
+        <li>Jira/Asana</li>
       </ul>
 
-      
       <p id="link-line">
         You can find my full resume{' '}
         <a
           target="_blank"
-          href="https://drive.google.com/file/d/1yDTvJC3a55dGo_Im9LBA75LMep06iwuC/view?usp=sharing"
+          href="https://drive.google.com/file/d/1fmxthz63-iYAEdHYjbZmhPN5WnRJbEE6/view?usp=sharing"
         >
           here
         </a>
